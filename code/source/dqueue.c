b93d8bcda835b3c4d266e93c40747d8e9e8b1d31
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-13 06:29:48 -0600

    Update dqueue.c

--- code/source/dqueue.c
+++ code/source/dqueue.c
@@ -10,8 +10,8 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-#include "fossil/xstructures/dqueue.h"
-#include "fossil/xstructures/common.h"
+#include "fossil/structures/dqueue.h"
+#include "fossil/common/common.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
