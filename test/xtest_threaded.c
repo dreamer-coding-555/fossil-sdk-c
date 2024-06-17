d59c587d9470df332b324dcc2cec5bbe07a982db
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-17 17:37:23 -0600

    Update xtest_threaded.c

--- test/xtest_threaded.c
+++ test/xtest_threaded.c
@@ -22,6 +22,15 @@ Description:
 // mock objects are set here.
 // * * * * * * * * * * * * * * * * * * * * * * * *
 
+// * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test classic threads
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+XTASK(pizza_maker_task, arg) {
+    // Making a cool pizza
+    (void)arg;
+}
+
 FOSSIL_FIXTURE(c_thread_fixture);
 FOSSIL_SETUP(c_thread_fixture) {
     // Setup code goes here
@@ -29,15 +38,6 @@ FOSSIL_SETUP(c_thread_fixture) {
 
 FOSSIL_TEARDOWN(c_thread_fixture) {
     // Teardown code goes here
-}
-
-// * * * * * * * * * * * * * * * * * * * * * * * *
-// * Fossil Logic Test Cases
-// * * * * * * * * * * * * * * * * * * * * * * * *
-
-XTASK(pizza_maker_task, arg) {
-    // Making a cool pizza
-    (void)arg;
 }
 
 // Test case 1: Test fossil_thread_create with valid parameters
@@ -57,10 +57,65 @@ FOSSIL_TEST(testing_thread_attr_create) {
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
+// * Fossil Logic Test thread-local
+// * * * * * * * * * * * * * * * * * * * * * * * *
+
+// Test fixture for thread-local storage
+FOSSIL_FIXTURE(c_thread_local_fixture);
+FOSSIL_SETUP(c_thread_local_fixture) {
+    // Setup code if needed
+}
+
+FOSSIL_TEARDOWN(c_thread_local_fixture) {
+    // Teardown code if needed
+}
+
+// Test case: Create and erase thread-local storage key
+FOSSIL_TEST(test_fossil_thread_local_create_erase) {
+    fossil_xthread_local_t key;
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_create(&key, NULL));
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_erase(key));
+}
+
+// Test case: Set and get thread-local storage value
+FOSSIL_TEST(test_fossil_thread_local_set_get) {
+    fossil_xthread_local_t key;
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_create(&key, NULL));
+    
+    const char *value = "test_value";
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_set(key, value));
+    
+    const char *retrieved_value = (const char*)fossil_thread_local_get(key);
+    ASSUME_ITS_EQUAL_S(value, retrieved_value);
+    
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_erase(key));
+}
+
+FOSSIL_TEST(test_fossil_thread_local_with_destructor) {
+    fossil_xthread_local_t key;
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_create(&key, dummy_destructor));
+    
+    char *value = malloc(20);
+    strcpy(value, "test_value_with_destructor");
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_set(key, value));
+    
+    const char *retrieved_value = (const char*)fossil_thread_local_get(key);
+    ASSUME_ITS_EQUAL_S(value, retrieved_value);
+    
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_local_erase(key));
+    dummy_destructor(value);
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
 FOSSIL_TEST_GROUP(c_threaded_tests) {
     // Threads Fixture
     ADD_TESTF(testing_thread_create, c_thread_fixture);
     ADD_TESTF(testing_thread_attr_create, c_thread_fixture);
+    
+    // Local Threads Fixture
+    ADD_TESTF(test_fossil_thread_local_create_erase, c_thread_local_fixture);
+    ADD_TESTF(test_fossil_thread_local_set_get, c_thread_local_fixture);
+    ADD_TESTF(test_fossil_thread_local_with_destructor, c_thread_local_fixture);
 } // end of fixture
