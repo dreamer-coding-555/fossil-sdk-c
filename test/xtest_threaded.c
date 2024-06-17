a502dab282c3f7cff7b00c12f0ff2eb92909d6bd
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-17 16:17:32 -0600

    Update xtest_threaded.c

--- test/xtest_threaded.c
+++ test/xtest_threaded.c
@@ -49,23 +49,14 @@ FOSSIL_TEST(test_fossil_thread_create_valid_params) {
     fossil_thread_detach(thread); // Detach the thread to clean up resources
 }
 
-// Test case 2: Test fossil_thread_join with valid parameters
-FOSSIL_TEST(test_fossil_thread_join_valid_params) {
-    fossil_xthread_t thread;
-    fossil_xtask_t task = {pizza_maker_task, NULL}; // Initialize task variable
-    fossil_thread_create(&thread, NULL, task); // Create a thread before joining it
-    fossil_thread_create(&thread, NULL, task); // Create a thread before joining it
-    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_join(thread, NULL));
-}
-
-// Test case 3: Test fossil_thread_attr_create
+// Test case 2: Test fossil_thread_attr_create
 FOSSIL_TEST(test_fossil_thread_attr_create) {
     fossil_xthread_attr_t attr;
     ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_attr_create(&attr));
     ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_attr_erase(&attr)); // Clean up after creating thread attributes
 }
 
-// Test case 4: Test fossil_thread_attr_erase with valid parameters
+// Test case 3: Test fossil_thread_attr_erase with valid parameters
 FOSSIL_TEST(test_fossil_thread_attr_erase_valid_params) {
     fossil_xthread_attr_t attr;
     ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_thread_attr_create(&attr)); // Create thread attributes before erasing them
@@ -78,7 +69,6 @@ FOSSIL_TEST(test_fossil_thread_attr_erase_valid_params) {
 FOSSIL_TEST_GROUP(c_threaded_tests) {
     // Threads Fixture
     ADD_TESTF(test_fossil_thread_create_valid_params, c_thread_fixture);
-    ADD_TESTF(test_fossil_thread_join_valid_params, c_thread_fixture);
     ADD_TESTF(test_fossil_thread_attr_create, c_thread_fixture);
     ADD_TESTF(test_fossil_thread_attr_erase_valid_params, c_thread_fixture);
 } // end of fixture
