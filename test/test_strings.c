9c811ac36221f57ec978149b6152b0b998544802
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-18 12:01:01 -0600

    Update test_strings.c

--- test/test_strings.c
+++ test/test_strings.c
@@ -45,8 +45,8 @@ FOSSIL_TEARDOWN(c_string_fixture) {
 // Test case 1: Test fossil_cstring_create
 FOSSIL_TEST(test_fossil_cstring_create) {
     cstring cstring;
-    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_cstr_create(&cstring));
-    fossil_cstr_erase(&cstring); // Clean up after creating a cstring
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_cstr_create(cstring));
+    fossil_cstr_erase(cstring); // Clean up after creating a cstring
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -64,8 +64,8 @@ FOSSIL_TEARDOWN(w_string_fixture) {
 // Test case 1: Test fossil_wstring_create
 FOSSIL_TEST(test_fossil_wstring_create) {
     wstring wstring;
-    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_wstr_create(&wstring));
-    fossil_wstr_erase(&wstring); // Clean up after creating a wstring
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_wstr_create(wstring));
+    fossil_wstr_erase(wstring); // Clean up after creating a wstring
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -83,8 +83,8 @@ FOSSIL_TEARDOWN(b_string_fixture) {
 // Test case 1: Test fossil_bstring_create
 FOSSIL_TEST(test_fossil_bstring_create) {
     bstring bstring;
-    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_bstr_create(&bstring));
-    fossil_bstr_erase(&bstring); // Clean up after creating a bstring
+    ASSUME_ITS_EQUAL_I32(FOSSIL_SUCCESS, fossil_bstr_create(bstring));
+    fossil_bstr_erase(bstring); // Clean up after creating a bstring
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
