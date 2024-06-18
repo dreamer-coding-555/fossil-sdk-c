e87ef7b3a21a5a0abcb4ad105633620469572036
Michael Gene Brockus <michaelbrockus@gmail.com>
2024-06-18 15:06:08 -0600

    add some new test

--- test/test_strings.c
+++ test/test_strings.c
@@ -45,8 +45,32 @@ FOSSIL_TEARDOWN(c_string_fixture) {
 // Test case 1: Test fossil_cstring_create
 FOSSIL_TEST(test_fossil_cstring_create) {
     cstring cstring = cnullptr;
-    ASSUME_ITS_TRUE(cnullptr == fossil_cstr_create(cstring));
+    ASSUME_ITS_TRUE(cnullptr == fossil_cstr_create(&cstring));
     fossil_cstr_erase(cstring); // Clean up after creating a cstring
+}
+
+// Test case 2: Test fossil_cstring_length
+FOSSIL_TEST(test_fossil_cstring_length) {
+    cstring cstring = "Hello, World!";
+    ASSUME_ITS_TRUE(fossil_cstr_length(cstring) == 13);
+}
+
+// Test case 3: Test fossil_cstring_copy
+FOSSIL_TEST(test_fossil_cstring_copy) {
+    cstring src = "Hello, World!";
+    cstring dest = cnullptr;
+    fossil_cstr_copy(dest, src);
+    ASSUME_ITS_TRUE(fossil_cstr_compare(src, dest) == 0);
+    fossil_cstr_erase(dest); // Clean up after copying
+}
+
+// Test case 4: Test fossil_cstring_concatenate
+FOSSIL_TEST(test_fossil_cstring_concatenate) {
+    cstring str1 = "Hello, ";
+    cstring str2 = "World!";
+    cstring result = fossil_cstr_concat(str1, str2);
+    ASSUME_ITS_TRUE(fossil_cstr_compare(result, "Hello, World!") == 0);
+    fossil_cstr_erase(result); // Clean up after concatenating
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -68,6 +92,30 @@ FOSSIL_TEST(test_fossil_wstring_create) {
     fossil_wstr_erase(wstring); // Clean up after creating a wstring
 }
 
+// Test case 2: Test fossil_wstring_length
+FOSSIL_TEST(test_fossil_wstring_length) {
+    wstring wstring = L"Hello, World!";
+    ASSUME_ITS_TRUE(fossil_wstr_length(wstring) == 13);
+}
+
+// Test case 3: Test fossil_wstring_copy
+FOSSIL_TEST(test_fossil_wstring_copy) {
+    wstring src = L"Hello, World!";
+    wstring dest = cnullptr;
+    fossil_wstr_copy(dest, src);
+    ASSUME_ITS_TRUE(fossil_wstr_compare(src, dest) == 0);
+    fossil_wstr_erase(dest); // Clean up after copying
+}
+
+// Test case 4: Test fossil_wstring_concatenate
+FOSSIL_TEST(test_fossil_wstring_concatenate) {
+    wstring str1 = L"Hello, ";
+    wstring str2 = L"World!";
+    wstring result = fossil_wstr_concat(str1, str2);
+    ASSUME_ITS_TRUE(fossil_wstr_compare(result, L"Hello, World!") == 0);
+    fossil_wstr_erase(result); // Clean up after concatenating
+}
+
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test byte string
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -87,6 +135,30 @@ FOSSIL_TEST(test_fossil_bstring_create) {
     fossil_bstr_erase(bstring); // Clean up after creating a bstring
 }
 
+// Test case 2: Test fossil_bstring_length
+FOSSIL_TEST(test_fossil_bstring_length) {
+    bstring bstring = "Hello, World!";
+    ASSUME_ITS_TRUE(fossil_bstr_length(bstring) == 13);
+}
+
+// Test case 3: Test fossil_bstring_copy
+FOSSIL_TEST(test_fossil_bstring_copy) {
+    bstring src = "Hello, World!";
+    bstring dest = cnullptr;
+    fossil_bstr_copy(dest, src);
+    ASSUME_ITS_TRUE(fossil_bstr_compare(src, dest) == 0);
+    fossil_bstr_erase(dest); // Clean up after copying
+}
+
+// Test case 4: Test fossil_bstring_concatenate
+FOSSIL_TEST(test_fossil_bstring_concatenate) {
+    bstring str1 = "Hello, ";
+    bstring str2 = "World!";
+    bstring result = fossil_bstr_concat(str1, str2);
+    ASSUME_ITS_TRUE(fossil_bstr_compare(result, "Hello, World!") == 0);
+    fossil_bstr_erase(result); // Clean up after concatenating
+}
+
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
@@ -94,10 +166,19 @@ FOSSIL_TEST(test_fossil_bstring_create) {
 FOSSIL_TEST_GROUP(c_strings_tests) {    
     // Classic Strings Fixture
     ADD_TESTF(test_fossil_cstring_create, c_string_fixture);
+    ADD_TESTF(test_fossil_cstring_length, c_string_fixture);
+    ADD_TESTF(test_fossil_cstring_copy, c_string_fixture);
+    ADD_TESTF(test_fossil_cstring_concatenate, c_string_fixture);
 
     // Wide Strings Fixture
     ADD_TESTF(test_fossil_wstring_create, w_string_fixture);
+    ADD_TESTF(test_fossil_wstring_length, w_string_fixture);
+    ADD_TESTF(test_fossil_wstring_copy, w_string_fixture);
+    ADD_TESTF(test_fossil_wstring_concatenate, w_string_fixture);
 
     // Byte Strings Fixture
     ADD_TESTF(test_fossil_bstring_create, b_string_fixture);
+    ADD_TESTF(test_fossil_bstring_length, b_string_fixture);
+    ADD_TESTF(test_fossil_bstring_copy, b_string_fixture);
+    ADD_TESTF(test_fossil_bstring_concatenate, b_string_fixture);
 } // end of tests
