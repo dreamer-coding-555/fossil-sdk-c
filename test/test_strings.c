6ef5ccc5c23581e21b459dd4dc6c70a9fd97936e
Michael Gene Brockus <michaelbrockus@gmail.com>
2024-06-19 08:53:31 -0600

    fix string constructor logic

--- test/test_strings.c
+++ test/test_strings.c
@@ -51,15 +51,15 @@ FOSSIL_TEST(test_fossil_cstring_create) {
 
 // Test case 2: Test fossil_cstring_create with a string
 FOSSIL_TEST(test_fossil_cstring_create_with_value) {
-    cstring var = "Pizza time!";
-    ASSUME_ITS_EQUAL_CSTR("Pizza time!", fossil_cstr_create(var));
+    cstring var = fossil_cstr_create("Pizza time!");
+    ASSUME_ITS_EQUAL_CSTR("Pizza time!", var);
     fossil_cstr_erase(var); // Clean up after creating a cstring
 }
 
 // Test case 3: Test fossil_cstring_create with a string and length
 FOSSIL_TEST(test_fossil_cstring_create_with_value_and_length) {
-    cstring var = "Pizza";
-    ASSUME_ITS_EQUAL_CSTR("Pizza", fossil_cstr_create(var));
+    cstring var = fossil_cstr_create("Pizza");
+    ASSUME_ITS_EQUAL_CSTR("Pizza", var);
     ASSUME_ITS_EQUAL_SIZE(5, fossil_cstr_length(var));
     fossil_cstr_erase(var); // Clean up after creating a cstring
 }
@@ -85,15 +85,15 @@ FOSSIL_TEST(test_fossil_wstring_create) {
 
 // Test case 2: Test fossil_wstring_create with a string
 FOSSIL_TEST(test_fossil_wstring_create_with_value) {
-    wstring var = L"Pizza time!";
-    ASSUME_ITS_EQUAL_WSTR(L"Pizza time!", fossil_wstr_create(var));
+    wstring var = fossil_wstr_create(L"Pizza time!");
+    ASSUME_ITS_EQUAL_WSTR(L"Pizza time!", var);
     fossil_wstr_erase(var); // Clean up after creating a wstring
 }
 
 // Test case 3: Test fossil_wstring_create with a string and length
 FOSSIL_TEST(test_fossil_wstring_create_with_value_and_length) {
-    wstring var = L"Pizza";
-    ASSUME_ITS_EQUAL_WSTR(L"Pizza", fossil_wstr_create(var));
+    wstring var = fossil_wstr_create(L"Pizza");
+    ASSUME_ITS_EQUAL_WSTR(L"Pizza", var);
     ASSUME_ITS_EQUAL_SIZE(5, fossil_wstr_length(var));
     fossil_wstr_erase(var); // Clean up after creating a wstring
 }
@@ -119,15 +119,15 @@ FOSSIL_TEST(test_fossil_bstring_create) {
 
 // Test case 2: Test fossil_bstring_create with a string
 FOSSIL_TEST(test_fossil_bstring_create_with_value) {
-    bstring var = FOSSIL_STATIC_CAST(bstring, "Pizza time!");
-    ASSUME_ITS_EQUAL_BSTR("Pizza time!", fossil_bstr_create(var));
+    bstring var = fossil_bstr_create(FOSSIL_STATIC_CAST(bstring, "Pizza time!"));
+    ASSUME_ITS_EQUAL_BSTR("Pizza time!", var);
     fossil_bstr_erase(var); // Clean up after creating a bstring
 }
 
 // Test case 3: Test fossil_bstring_create with a string and length
 FOSSIL_TEST(test_fossil_bstring_create_with_value_and_length) {
-    bstring var = FOSSIL_STATIC_CAST(bstring, "Pizza");
-    ASSUME_ITS_EQUAL_BSTR("Pizza", fossil_bstr_create(var));
+    bstring var = fossil_bstr_create(FOSSIL_STATIC_CAST(bstring, "Pizza"));
+    ASSUME_ITS_EQUAL_BSTR("Pizza", var);
     ASSUME_ITS_EQUAL_SIZE(5, fossil_bstr_length(var));
     fossil_bstr_erase(var); // Clean up after creating a bstring
 }
